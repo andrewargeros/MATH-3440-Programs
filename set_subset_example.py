b3da06dc55e13e242a7943de77e67f8fe414e542
Andrew Argeros <aargeros15@gmail.com>
2021-10-11 11:02:51 -0500

    Update set_subset_example.py

--- set_subset_example.py
+++ set_subset_example.py
@@ -3,17 +3,17 @@ a = [1,2,3,4]
 set_a = set(a)
 
 # Try Set Subset
-if set([]).issubset(set_a):
-    print("Empty set is a subset of set_a")
+if set([]).issubset(set_a): # True
+    print("Empty set is a subset of set_a") 
 
 # Try Set Membership
-if set([]) in set_a:
+if set([]) in set_a: # False
     print("Empty set is in set_a")
 else:
   print("Empty set is not in set_a")
 
 # Try List Membership
 try:
-    [] in set_a
+    [] in set_a # Error
 except:
     print("Empty List is not in List A")
