cc0dd0c4569344e109f6e5b24e5ccb40bcf12e4d
Andrew Argeros <aargeros15@gmail.com>
2021-10-18 11:45:59 -0500

    Create prime_numbers.py

---
+++ prime_numbers.py
@@ -0,0 +1,11 @@
+# Program to check if a number is prime or not
+
+from math import sqrt
+
+def check_prime(num):
+    for i in range(2, int(sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+print(check_prime(7919))
